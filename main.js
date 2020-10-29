ff6af9a76272791cc897c3a5801394596a6ede9e
MaxTarasevich <maxtarasevich@gmail.com>
2020-10-29 22:57:23 +0300

    HOVER, hover everywhere!11

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 /*********************** slider*******************************/
     let sliderItem = document.getElementsByClassName('slider-item');
-    console.log(sliderItem)
     for(i=0;i<sliderItem.length;i++){
         sliderItem[i].style.order = i;
         sliderItem[i].addEventListener('click',function(){
             let sliderElem = this;
             let order = sliderElem.style.order;
-            console.log( order);
-            console.log( sliderElem);
             for(i=0;i<sliderItem.length;i++){
                 if(sliderItem[i].style.order == 2){
                     sliderItem[i].style.order = order;
