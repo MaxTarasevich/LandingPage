23d9b024fbe8c388cfd5674f7ab8418898f141c0
MaxTarasevich <maxtarasevich@gmail.com>
2020-11-08 15:00:50 +0300

    add JS add CSS transition for modalWindow

--- main.js
+++ main.js
@@ -18,8 +18,34 @@
     /**********************login*************************** */
     let logIn = document.getElementById('login');
     let modalLogin = document.getElementsByClassName('modal-login');
+    let modalLoginSubmit = document.getElementById('modal-login_submit');
+    let modalLoginClose = document.getElementById('close-modal');
+    let UserName = document.getElementById('userName-login');
+    let LogOut = document.getElementsByClassName('LogOut');
+
     logIn.addEventListener('click',()=>{
-        console.log(modalLogin)
-        modalLogin.style.visibility = "visible"
-        
+        modalLogin[0].classList.remove('modal-login_open');
+        document.body.style.overflowY = "hidden";
+        document.body.style.paddingRight = "15px"
+    });
+
+    modalLoginClose.addEventListener('click',()=>{
+        modalLogin[0].classList.add('modal-login_open');
+        document.body.style.overflowY = "auto";
+        document.body.style.paddingRight = "0px" ;
+    });
+
+    modalLoginSubmit.addEventListener('click',()=>{
+        console.log(UserName.value);
+        logIn.innerText = `Добро пожаловать ${UserName.value}`;
+        LogOut[0].style.display = "block";
+        modalLogin[0].classList.add('modal-login_open');
+        document.body.style.overflowY = "auto";
+        document.body.style.paddingRight = "0px" ;
+    });
+
+    LogOut[0].addEventListener('click', function (){
+        LogOut[0].style.display = "none";
+        logIn.innerText = `LogIn`;
     })
+    
